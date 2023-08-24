eca12ab0c2f67ea9584437c3772d7f880ac156c5
Bruno Costa <bruno.costa@dafiti.com.br>
2023-08-24 16:03:32 -0300

    finalizado

--- project/src/Core/Modules/Category/Domain/Entities/Category.php
+++ project/src/Core/Modules/Category/Domain/Entities/Category.php
@@ -2,17 +2,21 @@
 
 namespace Core\Modules\Category\Domain\Entities;
 
+use Core\Shared\Domain\ValueObjects\Uuid;
 use DateTime;
 
 class Category
 {
     public function __construct(
-        protected string $name,
-        protected ?string $description = null,
-        protected bool $isActive = true,
+        protected string    $name,
+        protected ?string   $description = null,
+        protected bool      $isActive = true,
         protected ?DateTime $createdAt = null,
-    ) {
+        protected ?Uuid     $id = null,
+    )
+    {
         $this->createdAt = $createdAt ?: new DateTime();
+        $this->id = $this->id ?: Uuid::random();
     }
 
     public function __get($name)
@@ -23,5 +27,10 @@ class Category
     public function createdAt(): string
     {
         return $this->createdAt->format('Y-m-d H:i:s');
+    }
+
+    public function id(): string
+    {
+        return $this->id;
     }
 }
--- project/tests/Unit/Core/Modules/Category/Domain/Entities/CategoryTest.php
+++ project/tests/Unit/Core/Modules/Category/Domain/Entities/CategoryTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Core\Modules\Category\Domain\Entities\Category;
+use Core\Shared\Domain\ValueObjects\Uuid;
+use Ramsey\Uuid\Uuid as RamseyUuid;
 
 describe("Category Unit Test", function () {
     beforeEach(fn() => $this->category = new Category(
@@ -10,6 +12,13 @@ describe("Category Unit Test", function () {
 
     test("constructor of category, getters and setters", function () {
         expect($this->category->name)->not->toBeNull()
+            ->and($this->category)->toHaveProperties([
+                'id',
+                'name',
+                'description',
+                'isActive',
+                'createdAt'
+            ])
             ->and($this->category->name)->toBeString()
             ->and($this->category->name)->toBe('testing category')
             ->and($this->category->description)->toBe('testing description')
@@ -17,18 +26,22 @@ describe("Category Unit Test", function () {
             ->and($this->category->isActive)->toBeBool()
             ->and($this->category->createdAt)->not->toBeNull()
             ->and($this->category->createdAt)->toBeInstanceOf(DateTime::class)
-            ->and($this->category->createdAt())->toBeString();
+            ->and($this->category->createdAt())->toBeString()
+            ->and($this->category->id)->toBeInstanceOf(Uuid::class)
+            ->and($this->category->id())->toBeString();
 
         $category = new Category(
             name: 'testing category',
             isActive: false,
-            createdAt: $date = new DateTime('2020-01-01')
+            createdAt: $date = new DateTime('2020-01-01'),
+            id: new Uuid($id = RamseyUuid::uuid4()->toString())
         );
 
         expect($category->description)->toBeNull()
             ->and($category->isActive)->toBeFalse()
             ->and($category->isActive)->toBeBool()
             ->and($category->createdAt)->toBe($date)
-            ->and($category->createdAt())->toBe($date->format('Y-m-d H:i:s'));
+            ->and($category->createdAt())->toBe($date->format('Y-m-d H:i:s'))
+            ->and($category->id())->toBe($id);
     });
 });
